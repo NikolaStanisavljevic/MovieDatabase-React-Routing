c844d463264ea4d363f9108d866fc58dc937c94e
Nikola Stanisavljevic <marinko.magla91@gmail.com>
2018-05-11 18:02:34 +0200

    Create boostrap navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,12 +4,28 @@ import { Link } from 'react-router-dom';
 
 const navBar = (props) => {
   return (
-    <div className='nav'>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/collection"> My Collection</Link></li>
-        </ul>
+    <nav class="navbar navbar-light navbar-toggleable-md">
+  <div class="container">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
+    <a class="navbar-brand" href="#">Nikola Stanisavljevic</a>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav ">
+        <li class="nav-item mr-2">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item mr-2">
+          <a class="nav-link" href="#">Skills</a>
+        </li>
+        <li class="nav-item mr-2">
+          <a class="nav-link" href="#">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
   )
 }
 
